755316bde305d9d8ca2226dbed46a9b18d9e4442
Muhammad Saad <saad.salman11123@gmail.com>
2024-03-06 22:09:29 +0500

    fe and be done

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { Appbar } from "../components/Appbar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
+import axios from "axios"
+
+export const Dashboard = () => {
+    const [balance, setBalance] = useState(0);
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/account/balance", {
+            headers: {
+                Authorization: "Bearer " + localStorage.token
+            }
+        })
+            .then(response => {
+                setBalance(response.data.balance);
+            })
+    }, [])
+
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={balance} />
+            <Users />
+        </div>
+    </div>
+}
